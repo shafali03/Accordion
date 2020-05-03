6c7a6f97baed97ca7b19374142bb4182bd660ba9
shafali03 <shaf03sm@gmail.com>
2020-05-03 09:51:32 +0100

    refactor: converted the nodelist into an Array to use the forEach loop then added an event listener to the header

--- js/main.js
+++ js/main.js
@@ -1,43 +1,12 @@
 
-const accordions = document.querySelectorAll('.accordion')
+const accordions = Array.from(document.querySelectorAll('.accordion'))
 
-// Finds each accordion
+accordions.forEach(accordion => {
 
-const firstAccordion = accordions[0]
-const secondAccordion = accordions[1]
-const thirdAccordion = accordions[2]
-const fourthAccordion = accordions[3]
+  const accordionHeader = accordion.querySelector('.accordion__header')
 
+  accordionHeader.addEventListener('click', event => {
 
-// Find header for each accordion
-
-const firstAccordionHeader = firstAccordion.querySelector('.accordion__header')
-
-const secondAccordionHeader = secondAccordion.querySelector('.accordion__header')
-
-const thirdAccordionHeader = thirdAccordion.querySelector('.accordion__header')
-
-const fourthAccordionHeader = fourthAccordion.querySelector('.accordion__header')
-
-
-// Adds event listeners to all accordion headers
-
-
-firstAccordionHeader.addEventListener('click', event => {
-  firstAccordion.classList.toggle('is-open')
-})
-
-
-secondAccordionHeader.addEventListener('click', event => {
-  secondAccordion.classList.toggle('is-open')
-})
-
-
-thirdAccordionHeader.addEventListener('click', event => {
-  thirdAccordion.classList.toggle('is-open')
-})
-
-
-fourthAccordionHeader.addEventListener('click', event => {
-  fourthAccordion.classList.toggle('is-open')
+    accordion.classList.toggle('is-open')
+  })
 })
